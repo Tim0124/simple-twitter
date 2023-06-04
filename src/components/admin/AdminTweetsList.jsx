d589afa51579b4100b8c6bc0a3a01b8a7a1dafb1
Lee0514 <iris8505145@yahoo.com.tw>
2023-06-05 02:22:39 +0800

    add Admin pages

---
+++ src/components/admin/AdminTweetsList.jsx
@@ -0,0 +1,9 @@
+import './AdminTweetsList.module.scss'
+
+export default function AdminTweetsList() {
+  return(
+    <section className={`${style.adminTweetsHeader}`}>
+      
+    </section>
+  )
+}
