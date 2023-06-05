8c31c69418e39c1974d50049d5a733c1377d6b1f
Lee0514 <iris8505145@yahoo.com.tw>
2023-06-06 02:58:18 +0800

    fix: merge erro

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,16 @@ import AdminForm from './components/admin/AdminForm'
 import Setting from './components/setting/SettingForm'
 import PopularUserList from './UIcomponents/layouts/PopularUserList'
 import PopularUser from 'UIcomponents/layouts/PopularUser';
-import MainTweets from 'components/main/MainTweets';
-
+import AdminTweetsList from 'components/admin/AdminTweetsList';
+import AdminTweetsItem from 'components/admin/AdminTweetsItem';
+import AdminTweetsCardList from './components/admin/AdminTweetsCardsList'
+import AdminTweetsCard from './components/admin/AdminTweetsCards'
 
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
-        <MainTweets />
+        <Register />
     </div>
     </BrowserRouter>
   );
