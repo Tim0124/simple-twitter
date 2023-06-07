e2976e0a2a13552d0502b29f8804ba2feb739995
Lee0514 <iris8505145@yahoo.com.tw>
2023-06-07 15:30:23 +0800

    feat: add login & register & admin API

--- src/UIcomponents/input/Input.jsx
+++ src/UIcomponents/input/Input.jsx
@@ -4,7 +4,7 @@ export default function Input({type, label, value, placeholder, onChange}) {
 return (
   <label>
     <div className={`${style.inputTitle}`}>{label}</div>
-    <input 
+    <input className={`${style.inputUI}`}
       type={type || 'text'}
       value={value || ''}
       placeholder={placeholder || ''}
--- src/api/auth.js
+++ src/api/auth.js
@@ -12,6 +12,24 @@ export const login = async ({ account, password }) => {
     if (token) {
       return { success: true, ...data }
     }
+    console.log(data)
+    return data
+  } catch (error) {
+    console.error('[Login Failed]', error)
+  }
+}
+
+export const adminLogin = async ({ account, password }) => {
+  try {
+    const response = await axios.post(`${authURL}/adminSignin `, { account, password })
+    const { data } = response
+
+    const { token } = data
+
+    if (token) {
+      return { success: true, ...data }
+    }
+    console.log(data)
 
     return data
   } catch (error) {
@@ -19,11 +37,24 @@ export const login = async ({ account, password }) => {
   }
 }
 
-// fetch('https://gentle-temple-56514.herokuapp.com/api/users/signin')
-//   .then((response) => {
-//     console.log(response);
-//   })
-//   .catch((error) => {
-//     console.log(`Error: ${error}`);
-//   })
+export const register = async ({ account, name, email, password, checkPassword }) => {
+  try {
+    const response = await axios.post(`${authURL}/users `, { account, name, email, password, checkPassword })
+    const { data } = response
+
+    const { token } = data
+
+    if (token) {
+      return { success: true, ...data }
+    }
+    console.log(data)
+
+    return data
+  } catch (error) {
+    console.error('[Register Failed]', error)
+  }
+}
+
+
+
 
