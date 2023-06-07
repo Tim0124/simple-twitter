a48c033905e1c28916e02d1669bf8a1ca1cf238d
Tim Kao <a0925172969@gmail.com>
2023-06-08 02:25:11 +0800

    feat: add user content view component

---
+++ src/components/user/UserLikeContent.jsx
@@ -0,0 +1,39 @@
+import style from './UserLikeContent.module.scss'
+import { ReactComponent as Like } from '../../assets/RedVector.svg'
+import { ReactComponent as Message } from '../../assets/message.svg'
+
+export default function UserLikeContent ({name, account, avatar, content, quantity, likeQuantity, time}) {
+
+  return (
+    <div className={`${style.mainTweetsContainer}`}>
+      <div className={`${style.mainTweetsList}`}>
+        <div className={`${style.mainTweetsLogo}`}>
+          <img src={avatar} className={`${style.mainTweetsImg}`} alt="" width='50px' heigh='50px' />
+        </div>
+        <div className={`${style.mainTweetsInfo}`}>
+          <div className={`${style.mainTweetsSecInfo}`}>
+            <div className={`${style.mainTweetsNameGroup}`}>
+              <div className={`${style.mainTweetsName}`}>{name}</div>
+              <div className={`${style.mainTweetsSmallAccount}`}>
+                <div className={`${style.mainTweetsAccount}`}>{account}</div>
+                <div className={`${style.mainTweetsdot}`}>・</div>
+                <div className={`${style.mainTweetsTime}`}>{time}</div>
+              </div>
+            </div>
+          </div>
+          <div className={`${style.mainTweetsContent}`}>{content}</div>
+          <div className={`${style.mainTweetsQuantityGroup}`}>
+            <div className={`${style.mainTweetsQuantity}`}>
+              <Message width='16px' height='16px'/>
+              <p>{quantity}</p>
+              </div>
+            <div className={`${style.mainTweetsLikeQuantity}`}>
+              <Like width='16px' height='16px'/>
+              <p>{likeQuantity}</p>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
