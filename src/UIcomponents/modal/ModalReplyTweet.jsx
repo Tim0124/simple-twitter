ae410991503b2b811f8da3f2e099ebd6a6d259fd
Tim Kao <a0925172969@gmail.com>
2023-06-08 16:21:44 +0800

    fix: modified component style

--- src/UIcomponents/modal/ModalReplyTweet.jsx
+++ src/UIcomponents/modal/ModalReplyTweet.jsx
@@ -1,12 +1,12 @@
-import style from './ModalPostTweet.module.scss'
+import style from './ModalReplyTweet.module.scss'
 import { ReactComponent as Arrow } from '../../assets/arrow.svg'
-import ModalContent from '../../components/modal/ModalContent'
+import ModalReplyContent from './ModalReplyContent'
 import Button from 'UIcomponents/buttons/Button'
 import { ReactComponent as Close } from '../../assets/Vector.svg'
 
-export default function PostTweet () {
+export default function ModalReplyTweet () {
   return (
-    <div className={`${style.postTweetContainer}`}>
+     <div className={`${style.postTweetContainer}`}>
       <header className={`${style.tweetsHeader}`}>
         <nav className={`${style.tweetsNavbar}`}>
           <div className={`${style.navTitle}`}>
@@ -14,14 +14,17 @@ export default function PostTweet () {
             <h1 className={`${style.titleClose}`}><Close style={{color:'#ff6600'}}/></h1>
           </div>
           <div className={`${style.navButton}`}>
-            <Button size='middle' text='推文'/>
+            <Button size='middle' text='回覆'/>
           </div>
         </nav>
       </header>
-      <ModalContent/>
+      <ModalReplyContent/>
       <div className={`${style.footerButtonItem}`}>
+        <footer className={`${style.footerText}`}>
+              <p>內容不可空白</p>
+            </footer>
         <div className={`${style.footerButton}`}>
-        <Button size='middle' text='推文'/>
+        <Button size='middle' text='回覆'/>
       </div>
       </div>
     </div>
