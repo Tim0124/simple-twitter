499c84fd134e8d971e01cf6fb307b8916348150d
Lee0514 <iris8505145@yahoo.com.tw>
2023-06-09 14:54:18 +0800

    feat: ModalUserInfo API

---
+++ src/api/allAPI.js
@@ -0,0 +1,19 @@
+import axios from "axios"
+
+const baseURL = 'https://gentle-temple-56514.herokuapp.com/api/tweets'
+
+const axiosInstance = axios.create({
+  baseURL
+})
+
+axiosInstance.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('authToken')
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`
+    }
+    return config
+  },
+  err => Promise.reject(err)
+)
+export const Tweets = axiosInstance
--- src/api/auth.js
+++ src/api/auth.js
@@ -6,14 +6,13 @@ export const login = async ({ account, password }) => {
   try {
     const response = await axios.post(`${authURL}/users/signin`, { account, password })
     const { data } = response
-
-    const { token } = data
+    const { token } = data.data
 
     if (token) {
-      return { success: true, ...data }
+      return { success: true, ...data.data }
     }
     console.log(data)
-    return data
+    return data.data
   } catch (error) {
     console.error('[Login Failed]', error)
   }
@@ -23,15 +22,13 @@ export const adminLogin = async ({ account, password }) => {
   try {
     const response = await axios.post(`${authURL}/adminSignin `, { account, password })
     const { data } = response
-
-    const { token } = data
-
+    const { token } = data.data
     if (token) {
-      return { success: true, ...data }
+      return { success: true, ...data.data }
     }
     console.log(data)
 
-    return data
+    return data.data
   } catch (error) {
     console.error('[Login Failed]', error)
   }
@@ -42,19 +39,26 @@ export const register = async ({ account, name, email, password, checkPassword }
     const response = await axios.post(`${authURL}/users `, { account, name, email, password, checkPassword })
     const { data } = response
 
-    const { token } = data
+    const { token } = data.data
+    debugger
 
     if (token) {
-      return { success: true, ...data }
+      return { success: true, ...data.data }
     }
     console.log(data)
 
-    return data
+    return data.data
   } catch (error) {
     console.error('[Register Failed]', error)
   }
 }
 
-
+export const checkPermission = async(authToken) => {
+  const response = await axios.get(`${authURL}/test-token`,{
+  header: {
+      Authorization: 'Bearer ' + authToken,
+    }
+  })
+}
 
 
